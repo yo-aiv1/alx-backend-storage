a8a1f16b845f47f8bdd0c8419688d03d16f5dfee
yahya <yahyaoulha0@gmail.com>
2024-05-23 12:36:10 +0100

    worked

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Unitest for utils"""
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test Access Nested Map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> Any:
+        """Test accessing a value in a nested map with a key path.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
